7fe10d91df585bd0a9876c3bf1b5882fa9b77b62
Jimmvu <jimmy.h.vu_23@outlook.com>
2022-05-06 12:12:22 -0700

    Converted location,item,npc class to work with previous code. Added ability to check spoken to supsects AB#431

---
+++ test/com/meowmivice/game/GameTest.java
@@ -0,0 +1,18 @@
+package com.meowmivice.game;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.Test
+    public void execute() {
+    }
+
+    @org.junit.Test
+    public void mapFileReader() {
+    }
+}
