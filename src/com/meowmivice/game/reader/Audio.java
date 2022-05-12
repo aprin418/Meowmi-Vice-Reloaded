06073aa72df1fb85bffd682f8826ba7db4a00f95
Aaron Prince <aprin418@gmail.com>
2022-05-12 12:27:11 -0700

    added basic volume logic AB#565

--- src/com/meowmivice/game/reader/Audio.java
+++ src/com/meowmivice/game/reader/Audio.java
@@ -3,10 +3,12 @@ import javax.sound.sampled.*;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.Format;
 
 public class Audio {
     static private Clip clip;
     static private AudioInputStream audioStream;
+    private static boolean isSound = true;
 
     //reads the file, gets the audio, opens the audio, sets it to loop, and starts playing it
     public static void audio() throws LineUnavailableException, UnsupportedAudioFileException, IOException {
@@ -16,6 +18,36 @@ public class Audio {
         clip.open(audioStream);
         clip.loop(Clip.LOOP_CONTINUOUSLY);
         clip.start();
+    }
+
+    // pause/unpause audio
+    static void toggleSound() {
+        if (isSound) {
+            clip.stop();
+            isSound = false;
+        }
+        else {
+            clip.start();
+            isSound = true;
+        }
+    }
+
+    // lower volume
+    public static void lowerSoundVolume() {
+        if (!isSound) {
+            toggleSound();
+        }
+        FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+        gainControl.setValue(-6.0f);
+    }
+
+    // increase volume
+    public static void  increaseSoundVolume() {
+        if (!isSound) {
+            toggleSound();
+        }
+        FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+        gainControl.setValue(+6.0f);
     }
 
     //stops audio and then closes the file
